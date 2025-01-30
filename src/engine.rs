72189ac5416ed062d4d989f09a7c0cd16743710d
Jorge Terence <jorge.terence@outlook.com>
2025-01-30 12:24:41 -0300

    split scene intialization and window creation

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,13 @@
-struct Scene {
+use winit::event_loop::EventLoop;
+
+use crate::renderer;
+
+pub const EARTH_GRAVITY: f32 = 9.81;
+
+pub struct Scene {
+    lifecycle: EventLoop<()>,
+    title: &'static str,
+
     environment: Vec<Environment>,
     bodies: Vec<Body>,
     interface: Vec<UI>,
@@ -6,17 +15,42 @@ struct Scene {
     gravity: f32,
 }
 
-struct Environment {
+impl Scene {
+    pub fn new(title: &'static str, gravity: f32) -> Self {
+        let event_loop = EventLoop::new().expect("Failed event loop init");
+
+        Self {
+            lifecycle: event_loop,
+            title,
+
+            environment: Vec::new(),
+            bodies: Vec::new(),
+            interface: Vec::new(),
+
+            gravity,
+        }
+    }
+
+    pub fn execute(self) {
+        // TODO: generate VBs for everything in the scene
+        let mut app = renderer::Program::new(&self.lifecycle, self.title);
+        self.lifecycle.run_app(&mut app).expect("Failed to run app");
+    }
+}
+
+pub struct Environment {
     friction: f32,
 }
 
-struct Body {
+pub struct Body {
     mass: f32, // >= 0
     acceleration: f32,
     velocity: (f32, f32, f32), // 3D vector
+    size: f32,
 }
 
-struct UI {
+pub struct UI {
+    // TODO: create ascii texture
     text: String,
     position: (f32, f32),
     border: bool,
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 mod engine;
 mod renderer;
 
-use glium::winit::event_loop::EventLoop;
-use renderer::Program;
-
 // static VERTEX_SRC: &str = include_str!("shaders/triangle-vertex.glsl");
 // static FRAGMENT_SRC: &str = include_str!("shaders/triangle-fragment.glsl");
 
 fn main() {
-    let event_loop = EventLoop::new().expect("Failed event loop init");
-    let mut app = Program::new(&event_loop, "OpenGL Oxidized");
-    event_loop.run_app(&mut app).expect("Failed to run app");
+    let scene = engine::Scene::new("OpenGL Oxidized", engine::EARTH_GRAVITY);
+    scene.execute();
 }
